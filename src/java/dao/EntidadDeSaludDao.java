953e93aa5f8332b28673405b3db064c917ddc26d
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-11 23:47:20 -0500

    feat: Interfaz DAO para entidades de salud

---
+++ src/java/dao/EntidadDeSaludDao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import modelo.EntidadDeSalud;
+/**
+ *
+ * @author Usuario
+ */
+public interface EntidadDeSaludDao {
+    EntidadDeSalud buscarEntidadDeSaludPorId(int id);
+    boolean verificarSiEntidadDeSaludExiste(int id);
+    void guardar(EntidadDeSalud usuario,String tipo);
+    List<EntidadDeSalud> buscarEntidadDeSaludPorNombre(String tipo);
+    List<EntidadDeSalud> listarTodasLasEntidadDeSalud();
+    void actualizarEntidadDeSalud(int id, EntidadDeSalud usuario, String tipo);
+    void eliminarEntidadDeSalud(int id);
+    List<EntidadDeSalud> buscarEntidadDeSaludPorDireccion(String tipo);
+    List<EntidadDeSalud> buscarEntidadDeSaludPorGerente(int id);
+}
