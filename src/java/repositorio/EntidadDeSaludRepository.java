04a3fc4045cdba70a63017b6a86a0fbf475f5299
Ivan Dario Marchena Salgado <imarchenasalgado@gmail.com>
2025-11-11 23:48:00 -0500

    feat: Repositorio para gestión de entidades

---
+++ src/java/repositorio/EntidadDeSaludRepository.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositorio;
+import dao.EntidadDeSaludDao;
+import factory.EntidadDeSaludDaoFactory;
+import java.sql.SQLException;
+import java.util.List;
+import modelo.EntidadDeSalud;
+/**
+ *
+ * @author Usuario
+ */
+public class EntidadDeSaludRepository {
+    private final EntidadDeSaludDao entidadDeSaludDao;
+    public EntidadDeSaludRepository(String tipoDb) throws SQLException{    
+        entidadDeSaludDao = EntidadDeSaludDaoFactory.dao(tipoDb);
+    }
+    
+    public EntidadDeSalud buscarEntidadDeSaludPorId(int id) {
+        return entidadDeSaludDao.buscarEntidadDeSaludPorId(id);
+    }
+    
+    public boolean verificarSiEntidadDeSaludExiste(int id) {
+        return entidadDeSaludDao.verificarSiEntidadDeSaludExiste(id);
+    }
+    public void guardar(EntidadDeSalud entidadDeSalud, String identificador) {
+        entidadDeSaludDao.guardar(entidadDeSalud, identificador);
+    }
+    public List<EntidadDeSalud> listarTodasLasEntidadDeSalud() {
+        return entidadDeSaludDao.listarTodasLasEntidadDeSalud();
+    }
+    
+    public void actualizarEntidadDeSalud(int id, EntidadDeSalud entidadDeSalud, String tipo) {
+        entidadDeSaludDao.actualizarEntidadDeSalud(id, entidadDeSalud, tipo);
+    }
+    
+    public void eliminarEntidadDeSalud(int id) {
+        entidadDeSaludDao.eliminarEntidadDeSalud(id);
+    }
+    public List<EntidadDeSalud> buscarEntidadDeSaludPorNombre(String tipo){
+        return entidadDeSaludDao.buscarEntidadDeSaludPorNombre(tipo);
+    }
+    
+    public List<EntidadDeSalud> buscarEntidadDeSaludPorDireccion(String tipo){
+        return entidadDeSaludDao.buscarEntidadDeSaludPorDireccion(tipo);
+    }
+
+    public List<EntidadDeSalud> buscarEntidadDeSaludPorGerente(int idUsuarioGerente) {
+        return entidadDeSaludDao.buscarEntidadDeSaludPorGerente(idUsuarioGerente);
+    }
+
+    
+}
